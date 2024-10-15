f410f2df958367fc713253d514420d116beefd1b
Mishti Panchal <manishapanchal169@gmail.com>
2024-10-15 11:53:53 +0530

    refactor: :fire: remove old code

--- src/components/RestaurantCard/RestaurantCard.jsx
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -1,19 +1,5 @@
 import React from "react";
 
-// const RestaurantCard  = (props) => {
-// 	const { name, cuisines, avgRating, cloudinaryImageId } = props;
-
-// 	return (
-// 		<div className='card'>
-// 			<img src={`${IMG_CDN_URL}${cloudinaryImageId}`} />
-// 			<h2>{name}</h2>
-// 			<h3>{cuisines.join(',')}</h3>
-// 			<h4>{avgRating} stars</h4>
-// 		</div>
-// 	);
-// };
-// export default RestaurantCards;
-
 const RestaurantCard = (props) => {
   const { name, cuisines, avgRating, cloudinaryImageId } = props;
   console.log(props.info, "I AM PROPS");
